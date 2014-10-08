dcda132b5e1761e6b7a41402b8f7ad7e95a16ef2
John Crocker <johnniecrocker@yahoo.com>
2014-10-08 15:15:05 -0400

    Defined most repo variables

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,34 @@ var person_name,
     repo_bar,
     repo_url,
     repo_star,
+    repo_lang,
     repo_fork,
     photo_url,
     orgs;
+
+
+repos.forEach(function(repo){
+
+  //console.log(repo.name);
+
+  repo_title = repo.name;
+
+  //console.log(repo.updated_at);
+
+  //console.log(repo.description);
+
+  repo_desc = repo.description;
+
+  //console.log(repo.svn_url);
+
+  repo_url = repo.svn_url;
+
+  //console.log(repo.language);
+
+  repo_lang = repo.language;
+
+  repo_star = repo.stargazers_count;
+
+  repo_fork = repo.forks_count;
+
+})
