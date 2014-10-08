093d282f81c2dfad3d71b0d2e0465fcb37a24238
John Crocker <johnniecrocker@yahoo.com>
2014-10-08 14:48:19 -0400

    added js variables

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
 console.log('The Iron Yard Rocks');
+
+
+var person_name,
+    username,
+    location,
+    join,
+    repo_title,
+    repo_update,
+    repo_desc,
+    repo_bar,
+    repo_url,
+    repo_star,
+    repo_fork,
+    photo_url,
+    orgs;
--- app/scripts/org.js
+++ app/scripts/org.js
@@ -1,4 +1,4 @@
-{
+var org= [{
 "message": "API rate limit exceeded for 66.194.102.6. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)",
 "documentation_url": "https://developer.github.com/v3/#rate-limiting"
-}
+}]
