02d32ae210d5d7847fdfcd72660a088b8dcd03fb
John Crocker <johnniecrocker@yahoo.com>
2014-10-09 15:25:37 -0400

    org template working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,18 +5,18 @@ var api_repo = 'https://api.github.com/users/gt7348b/repos',
     api_user = 'https://api.github.com/users/gt7348b',
     api_org = 'https://api.github.com/users/gt7348b/orgs',
     api_starred = 'https://api.github.com/users/gt7348b/starred',
+    template_about = $('#about').html(),
+    template_repo = $('#repositories').html(),
+    template_photo = $('#sideid').html(),
+    template_header = $('#header_right').html(),
+    template_org = $('#organization').html(),
+    template_follow,
     render_about,
     render_header,
     render_org,
     render_photo,
     render_repo,
     render_starred,
-    template_about = $('#about').html(),
-    template_repo = $('#repositories').html(),
-    template_photo = $('#sideid').html(),
-    template_header = $('#header_right').html(),
-    template_org,
-    template_follow,
     person_name,
     username,
     loc,
@@ -36,11 +36,22 @@ var api_repo = 'https://api.github.com/users/gt7348b/repos',
 
     render_header = _.template(template_header);
 
+    render_org = _.template(template_org)
+
     render_photo = _.template(template_photo);
 
     render_repo = _.template(template_repo);
 
+$.getJSON(api_org).done( function(org_data){
 
+          org_data.forEach(function(o){
+
+            orgs = o.avatar_url;
+
+            $('.org_img').append(render_org(o));
+
+          })
+        });
 
 
 
